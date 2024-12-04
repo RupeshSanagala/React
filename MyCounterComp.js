cfe37933e8fd8c4dcc7f9df3fb8b0a1830ac49d9
RUPESH KUMAR REDDY SANAGALA <116737181+RupeshSanagala@users.noreply.github.com>
2024-12-04 07:11:40 +0530

    Add files via upload

--- MyCounterComp.js
+++ MyCounterComp.js
@@ -1,7 +1,46 @@
-import { Component } from "react";
+import React, { Component } from 'react'
 
-class MyCounterComp extends Component{
-    render(){
-        
+export default class MyCounterComp extends Component {
+    constructor(){
+        super();
+        this.state ={
+            count:0
+        }
     }
+
+    increment ()=>{
+        this.setState((prevState)=>({
+            {count:prevState.count+1,}
+
+        }));
+
+    }
+
+    decrement ()=>{
+        this.setState((prevState)=>({
+            {count:prevState.count-1,}
+
+        }));
+
+    }
+
+    reset ()=>{
+        this.setState(()=>({
+            {count: 0,}
+
+        }));
+
+    }
+
+
+  render() {
+    return (
+      <div>
+        <h1>This is MyCounterComp</h1>
+        <button type="buttom" onClick={this.increment()}>count++</button>
+        <button type="buttom" onClick={this.decrement()}>count--</button>
+        <button type="reset" onClick={this.reset()}>reset</button>
+      </div>
+    )
+  }
 }
